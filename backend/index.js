9a211c1ef23e1db5c9d2f309b5329aff1baf22b6
israel <adigun.jolly@gmail.com>
2022-01-09 12:26:25 +0100

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+
+
+const app = express();
+const server = http.createServer(app)
+
+app.use(cors());
+
+const io = require("socket.io")(server, {cors:{origin:"*"}})
+
+const { addUser, removeUser, getUsers, getUserById } = require("./user");
+
+io.on("connection", (socket) => {
+    socket.on("join", (user, callback) => {
+        const { error, profile } = addUser({id:socket.id, room:user.room, name:user.name});
+        if(error) return callback(error);
+        socket.join(user.room);
+
+        socket.emit("message", {user:{name:"admin"}, message:`Welcome ${user.name}`})
+        socket.broadcast.to(user.room).emit("message", {user:{name:"admin"}, message:`User ${user.name} has joined`})
+
+        io.to(user.room).emit("roomData", {users:getUsers(user.room)})
+
+        callback();
+
+    })
+
+    socket.on("sendMessage", (msg) => {
+        const userId = socket.id;
+        const user = getUserById(userId);
+        io.to(user.room).emit("message", {user:user, message:msg });
+    })
+
+    socket.on("disconnect", () => {
+        const user = getUserById(socket.id);
+        removeUser(user)
+        io.to(user.room).emit("message", {user:{name:"admin"}, message:`${user.name} has left the chat room`});
+    })
+})
+
+server.listen(process.env.port || 5000, () => console.log("Server has started"))
